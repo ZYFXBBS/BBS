6cc62831a07f45e315586ade08b23bedff641d43
zhp <zhp@shenhua>
2014-06-04 16:47:32 +0800

    增加分页

--- src/com/zyfx/core/bbs/member/inter/IMemberOperation.java
+++ src/com/zyfx/core/bbs/member/inter/IMemberOperation.java
@@ -1,8 +1,10 @@
 package com.zyfx.core.bbs.member.inter;
 
+import java.util.List;
 import java.util.Map;
 
 import com.zyfx.core.bbs.member.model.Member;
+import com.zyfx.core.framework.common.utils.Page;
 
 
 public interface IMemberOperation extends IBaseOperation {
@@ -12,5 +14,12 @@ public interface IMemberOperation extends IBaseOperation {
 	 * @param 
 	 * @author by zhp 2014-5-29 上午12:18:50
 	 */
-	public Map selectMemberByID(int memberId);
+	public Member selectMemberByID(int memberId);
+	
+	/**
+	 * 查询所有 会员 并且分页
+	 * @param page
+	 * @return
+	 */
+	public List<Member> queryAllMembers(Page page);
 }
