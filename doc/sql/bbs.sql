81a25749e40c6f07b496b74f411af04adfd15385
zhp <zhp@shenhua>
2014-06-06 14:25:20 +0800

    更新sql

--- doc/sql/bbs.sql
+++ doc/sql/bbs.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50150
 File Encoding         : 65001
 
-Date: 2014-06-04 12:01:32
+Date: 2014-06-06 14:24:39
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -20,13 +20,38 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 DROP TABLE IF EXISTS `member`;
 CREATE TABLE `member` (
-  `memberId` int(11) NOT NULL,
+  `memberId` int(11) NOT NULL AUTO_INCREMENT,
   `memberName` varchar(255) DEFAULT NULL,
   `memberPWD` varchar(255) DEFAULT NULL,
+  `memberRealName` varchar(255) DEFAULT NULL,
+  `memberIcon` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `mobile` varchar(255) DEFAULT NULL,
+  `homephone` varchar(255) DEFAULT NULL,
+  `qq` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `postCode` varchar(255) DEFAULT NULL,
+  `isAdmin` varchar(255) DEFAULT NULL,
+  `isPower` varchar(255) DEFAULT NULL,
+  `mark` varchar(255) DEFAULT NULL,
+  `action` varchar(255) DEFAULT NULL,
+  `topicCount` varchar(255) DEFAULT NULL,
+  `replyCount` varchar(255) DEFAULT NULL,
+  `date` datetime DEFAULT NULL,
+  `LastVist` datetime DEFAULT NULL,
   PRIMARY KEY (`memberId`)
-) ENGINE=InnoDB DEFAULT CHARSET=gb2312;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=gb2312;
 
 -- ----------------------------
 -- Records of member
 -- ----------------------------
-INSERT INTO `member` VALUES ('1', 'shenhua', 'shenhua');
+INSERT INTO `member` VALUES ('1', 'zhp1111111111111111', '1231111111', '22222222222222', null, null, null, null, null, null, null, null, null, null, null, '100', null, '2014-06-06 14:16:35', null);
+INSERT INTO `member` VALUES ('11', 'zhp', '123', '88', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `member` VALUES ('12', 'zhp', '123', 'sssssss', null, null, null, null, null, null, null, null, null, null, null, '0', '0', null, null);
+INSERT INTO `member` VALUES ('13', 'zhp', '123', 'sssssss', null, null, null, null, null, null, null, null, null, null, null, '0', '0', null, null);
+INSERT INTO `member` VALUES ('14', 'zhp', '123', 'sssssss', null, null, null, null, null, null, null, null, null, null, null, '0', '0', null, null);
+INSERT INTO `member` VALUES ('15', 'zhp', '123', 'sssssss', null, null, null, null, null, null, null, null, null, null, null, '0', '0', null, null);
+INSERT INTO `member` VALUES ('16', 'zhp', '123', 'sssssss', null, null, null, null, null, null, null, null, null, null, null, '0', '0', null, null);
+INSERT INTO `member` VALUES ('17', 'zhp', '123', 'sssssss', null, null, null, null, null, null, null, null, null, null, null, '0', '0', null, null);
+INSERT INTO `member` VALUES ('18', 'zhp', '123', 'sssssss', null, null, null, null, null, null, null, null, null, null, null, '0', '0', null, null);
+INSERT INTO `member` VALUES ('19', 'zhp', '123', 'sssssss', null, null, null, null, null, null, null, null, null, null, null, '0', '0', null, null);
