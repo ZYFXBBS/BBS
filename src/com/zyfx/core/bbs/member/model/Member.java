122af751400b9c632fd02bf271cf823500b286cc
zhp <zhp@shenhua>
2014-06-09 13:52:08 +0800

    修改配置 缓存策略

--- src/com/zyfx/core/bbs/member/model/Member.java
+++ src/com/zyfx/core/bbs/member/model/Member.java
@@ -1,8 +1,9 @@
 package com.zyfx.core.bbs.member.model;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class Member {
+public class Member implements Serializable{
 	
 	private int memberId;
 	private String memberName;//会员名称
